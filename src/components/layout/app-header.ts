d83d4999460f64e77f5661b73cd569e4bb9eda63
Carlos Alberto Pereira Gomes <carlosapgomes@users.noreply.github.com>
2026-04-02 18:50:19 -0300

    feat(ui): remove left-control border and thicken header back arrow

--- src/components/layout/app-header.ts
+++ src/components/layout/app-header.ts
@@ -324,13 +324,14 @@ export class AppHeader extends LitElement {
                 ${this.showBack
                   ? html`
                       <button
-                        class="btn wf-nav-left-control"
+                        class="wf-nav-left-control text-primary"
                         @click=${this.handleBackClick}
                         aria-label="Voltar"
                         title="Voltar"
+                        type="button"
                       >
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
+                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="3.5" d="M15 19l-7-7 7-7" />
                         </svg>
                       </button>
                     `
